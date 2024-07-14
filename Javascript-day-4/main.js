2f72c98479539993c0682084ecf07ad90b794fa0
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-07-14 10:28:24 +0500

    javascript day 4 completed

---
+++ Javascript-day-4/main.js
@@ -0,0 +1,25 @@
+//? Write a function called checkTriangle Type that takes three parameters representing the lengths of the sides of a triangle. The function should return a string indicating the type of triangle: "equilateral", "isosceles", or "scalene".
+
+// console.log(checkTriangleType(3, 3, 3)); // Output: "equilateral" console.log(checkTriangleType(3, 4, 3)); // Output: "isosceles" console.log(checkTriangleType(5, 8, 6)); // Output: "scalene"
+
+//todo The function should adhere to the following rules:
+
+//? If all three sides are of equal length, return "equilateral". 
+//? If only two sides are of equal length, return "isosceles". 
+//? If all three sides have different lengths, return "scalene".
+
+
+
+let checkTriangleType = (a, b, c) => {
+    // console.log(a, b, c);
+
+    if (a === b && b === c) {
+        console.log('this is equilateral');
+    } else if (a === b || a === c){
+        console.log('this is isosceles');
+    }else {
+        console.log('this is scalene');
+    }
+}
+
+checkTriangleType(3, 3, 3)
