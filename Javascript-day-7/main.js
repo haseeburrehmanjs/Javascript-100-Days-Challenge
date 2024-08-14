cddf1d5b758f1cb502883fdad42f24c0565f7044
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-08-14 14:52:35 +0500

    modified work

--- Javascript-day-7/main.js
+++ Javascript-day-7/main.js
@@ -8,8 +8,33 @@
 // console.log(findMax([-10, -2, -4, -9, -1]));
 // console.log(findMax([5]));
 
-let findMax = (arr) => {
-    return Math.max(...arr)
+// let findMax = (arr) => {
+//     return Math.max(...arr)
+// }
+
+// console.log(findMax([1,2,3,4,5,67]));
+// console.log(findMax([-10, -2, -4, -9, -1]));
+// console.log(findMax([5]));
+
+
+let form = document.querySelector('#form')
+let number = document.querySelector('#number')
+let h2 = document.querySelector('.number')
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    let number1 = number.value
+
+    function formattedNumber(num){
+        num = num.replace(/^0/, '+92')
+        h2.innerHTML = num
+    }
+    console.log(formattedNumber(number1));
+    
+})
+
+let findMax = (array) => {
+    return array.max(...array)
 }
 
 console.log(findMax([1,2,3,4,5,67]));
